73c44b40f08228ae4248e97291289cd7e0d6ccb5
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-09-07 15:47:39 -0700

    add strapi plugin

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -80,6 +80,29 @@ const plugins = [
       clientSecret: process.env.PAYPAL_CLIENT_SECRET,
       authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
     },
+  },  
+  {
+    resolve: "medusa-plugin-strapi-ts",
+    options: {
+      strapi_protocol: process.env.STRAPI_PROTOCOL,
+      strapi_host: process.env.STRAPI_SERVER_HOSTNAME,
+      strapi_port: process.env.STRAPI_PORT,
+      strapi_secret: process.env.STRAPI_SECRET,
+      strapi_default_user: {
+          username: process.env.STRAPI_MEDUSA_USER,
+          password: process.env.STRAPI_MEDUSA_PASSWORD,
+          email: process.env.STRAPI_MEDUSA_EMAIL,
+          confirmed: true,
+          blocked: false,
+          provider: "local",
+      },
+      strapi_admin: {
+          username: process.env.STRAPI_SUPER_USERNAME,
+          password: process.env.STRAPI_SUPER_PASSWORD,
+          email: process.env.STRAPI_SUPER_USER_EMAIL,
+      },
+      auto_start: true,
+    },
   },
 ];
 
